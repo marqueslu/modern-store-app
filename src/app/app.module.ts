aa34a370860eb00a575d8900840287c1e70c0bd4
marqueslu <lucas.msilva09@gmail.com>
2018-11-27 08:19:22 -0200

    Componentizando a aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadbarComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
