9d30747d2a5790f1ebaa95fa8b83d27cfd3cc7d2
marqueslu <lucas.msilva09@gmail.com>
2018-12-05 08:19:57 -0200

    Adicionando validações personalizadas na página de login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
+import { CustomValidator } from './../../validators/custom.validator';
+
+import { Ui } from './../../utils/ui';
+
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  providers: [Ui]
 })
 export class LoginPageComponent implements OnInit {
 
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private ui: Ui) {
     this.form = this.fb.group({
       email: ['', Validators.compose([
         Validators.minLength(5), 
         Validators.maxLength(160), 
-        Validators.required
+        Validators.required,
+        CustomValidator.EmailValidator
       ])],
       password: ['', Validators.compose([
         Validators.minLength(6),
@@ -27,6 +33,22 @@ export class LoginPageComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  checkEmail(){
+   this.ui.lock('emailControl');
+    setTimeout(() => {
+      this.ui.unlock('emailControl');
+      console.log(this.form.controls['email'].value);
+    }, 3000);
+  }
+
+  showModal(){
+    this.ui.setActive('modal');
+  }
+
+  hideModal(){
+    this.ui.setInactive('modal');
   }
 
 }
